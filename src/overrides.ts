9f1abbd1c5bbcfd340850afe022cc36bf44c9166
FrederikHartung <90627809+FrederikHartung@users.noreply.github.com>
2026-03-21 12:49:14 +0100

    Disable mystery encounters for seeded strategic pipeline on v1.12.0.10

--- src/overrides.ts
+++ src/overrides.ts
@@ -275,7 +275,7 @@ class DefaultOverrides {
    *
    * Note: Make sure `STARTING_WAVE_OVERRIDE > 10`, otherwise MEs won't trigger
    */
-  readonly MYSTERY_ENCOUNTER_RATE_OVERRIDE: number | null = null;
+  readonly MYSTERY_ENCOUNTER_RATE_OVERRIDE: number | null = 0;
   readonly MYSTERY_ENCOUNTER_TIER_OVERRIDE: MysteryEncounterTier | null = null;
   readonly MYSTERY_ENCOUNTER_OVERRIDE: MysteryEncounterType | null = null;
 
